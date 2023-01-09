c9bc84745539f7b1087948e852e9e7114b750d03
howdyrio <121068703+howdyrio@users.noreply.github.com>
2023-01-08 20:32:11 -0600

    Update pong-audio.js

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -49,7 +49,7 @@ soundArray.push(wallSound); //add sound to list of sounds
 export var paddleSound = new soundFile("ballbell.mp3");
 soundArray.push(paddleSound);
 
-export var scoreSound = new soundFile("goal2.mp3");
+export var scoreSound = new soundFile("scorepoint.mp3");
 soundArray.push(scoreSound);
 
 export var ambientSound = new soundFile("sevenhz.mp3");
