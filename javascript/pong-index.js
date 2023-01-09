5364b97987a35e9cf1d76d7ff5efdfce4e5b98cc
howdyrio <121068703+howdyrio@users.noreply.github.com>
2023-01-09 16:07:09 -0600

    Update pong-index.js

--- javascript/pong-index.js
+++ javascript/pong-index.js
@@ -74,6 +74,7 @@ function gameProcess() {
     leftScoreHigher.turnOn();
   }
 
+  /*
   ambientSound.player.playbackRate = scalerange(
     Math.abs(ball.direction.x),
     0,
@@ -81,6 +82,7 @@ function gameProcess() {
     1,
     3
   );
+  */
   //EVENTS
   // this is a non-persistant event so it only needs to be checked
   if (ballHitWall.state === true) {
